707958c02e6216876e99d0abf9aa56c7720e34d3
Marco Tuzza <marcotuzza@hotmail.it>
2025-02-02 02:27:59 +0400

    Refactor authentication to use username instead of email, update related models, migrations, and views

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $username = env('ADMIN_USERNAME');
+        $password = env('ADMIN_PASSWORD');
+
+        if ($username && $password) {
+            User::updateOrCreate([
+                'username' => $username,
+                'password' => Hash::make($password)
+            ]);
+        } else {
+            $this->command->info('ADMIN_EMAIL or ADMIN_PASSWORD are not set in .env file.');
+        }
+    }
+}
